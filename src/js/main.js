abbf1550d0b01782dbe17bd9053c71dccfa0bd06
Kashev Dalmia <kashev.dalmia@gmail.com>
2014-02-13 12:13:04 -0600

    updated readme, working skeleton server
    copied from previous projects. grunt, sass, bourbon, reset.css.

---
+++ src/js/main.js
@@ -0,0 +1,13 @@
+/*
+  Kashev Dalmia
+  kashev.dalmia@gmail.com
+
+  'Persona Prototyping : Designing for Designers'
+  or
+  'Fake Apps for Fake People'
+
+  A Project for ARTD 313 @ UIUC, Sp 2014
+  proj2b - dalmia3
+
+  main.js
+*/
